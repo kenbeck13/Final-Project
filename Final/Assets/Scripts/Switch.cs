ad980803553144cb0865087c3e320315d4096b37
kenbeck13 <kenbeck13@gmail.com>
2018-11-23 18:14:35 -0500

    Switches

---
+++ Final/Assets/Scripts/Switch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+	public bool isOn;
+	public bool isPermanent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			if (!isOn) {
+				isOn = true;
+			}
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			if (isOn && !isPermanent) {
+				isOn = false;
+			}
+		}
+	}
+}
