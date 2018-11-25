a64e966c0cd73f04c4308afcc8b7218ca317eec5
kenbeck13 <kenbeck13@gmail.com>
2018-11-25 18:06:18 -0500

    Click to point and pointerTimer

--- Final/Assets/Scripts/MousePointer.cs
+++ Final/Assets/Scripts/MousePointer.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class MousePointer : MonoBehaviour {
 
+	SpriteRenderer rend;
+	CircleCollider2D col;
+	public float pointerTimerMax;
+	float pointerTimer;
+
 	// Use this for initialization
 	void Start () {
-	
+		rend = GetComponent<SpriteRenderer> ();
+		col = GetComponent<CircleCollider2D> ();
+		rend.enabled = false;
+		col.enabled = false;
+		pointerTimer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			pointerTimer = pointerTimerMax;
+			rend.enabled = true;
+			col.enabled = true;
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			rend.enabled = false;
+			col.enabled = false;
+		}
+		if (pointerTimer < 0) {
+			rend.enabled = false;
+			col.enabled = false;
+		}
 		Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		transform.position = mousePos;
+		pointerTimer -= Time.deltaTime;
 	}
 }
